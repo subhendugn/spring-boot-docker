a3834ef4d62cf2144116a1a61e7bba68106aee99
Guha Neogi, Subhendu <Subhendu.GuhaNeogi@ellucian.com>
2020-02-27 11:29:55 +0530

    first commit

---
+++ src/main/java/com/test/dockertest/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.test.dockertest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("/api")
+	public String index() {
+		return "Hello Docker";
+	}
+
+}
