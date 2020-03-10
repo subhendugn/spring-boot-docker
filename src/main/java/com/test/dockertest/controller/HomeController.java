53c45bc115901613b1e56ee7347d754bc89b83b4
Guha Neogi, Subhendu <Subhendu.GuhaNeogi@ellucian.com>
2020-03-10 16:03:24 +0530

    Added Unit Test cases for index()

--- src/main/java/com/test/dockertest/controller/HomeController.java
+++ src/main/java/com/test/dockertest/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 	
-	@GetMapping("/api")
+	@GetMapping("api")
 	public String index() {
 		return "<h1>Hello From Spring Boot!!</h1>";
 	}
--- src/test/java/com/test/dockertest/DockerTestApplicationTests.java
+++ src/test/java/com/test/dockertest/DockerTestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.test.dockertest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.test.dockertest.controller.HomeController;
 
 @SpringBootTest
 class DockerTestApplicationTests {
+	
+	@Autowired
+	private HomeController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+		
+	}
+	
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+        String result = controller.index();
+        assertEquals(result, "<h1>Hello From Spring Boot!!</h1>");
 	}
 
 }
